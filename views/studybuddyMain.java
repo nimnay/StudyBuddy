74d22f0e69ea44312186338a9193f2daf8752366
Diana Sanchez <dianasanchezcruz17@gmail.com>
2025-08-28 15:53:24 -0400

    created real main rougly and views folder

---
+++ views/studybuddyMain.java
@@ -0,0 +1,17 @@
+package views;
+
+import java.util.Scanner;
+
+public class studybuddyMain {
+    public static void main(String [] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Study Buddy App\n");
+        System.out.println("In order to start, we need to collect information for your profile");
+
+        // after student class is created, i'll add scanner.next() for name, major and other info
+
+
+    }
+
+}
