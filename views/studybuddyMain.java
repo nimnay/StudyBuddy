86c18730a1a988caca166e216ee46f5e7e343656
Nimra Nayyar <nimranayyar05@gmail.com>
2025-08-28 18:55:15 -0400

    cleaning

--- views/studybuddyMain.java
+++ views/studybuddyMain.java
@@ -1,17 +1,31 @@
 package views;
 
-import java.util.Scanner;
+import models.Student;
 
-public class studybuddyMain {
-    public static void main(String [] args) {
+public class StudyBuddyMain {
+    public static void main(String[] args) {
+        // Create a student
+        /**Student s1 = new Student("Alice", "Computer Science");
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the Study Buddy App\n");
-        System.out.println("In order to start, we need to collect information for your profile");
+        // Test courses
+        System.out.println("=== Testing Courses ===");
+        s1.addCourse("CPSC 1010");
+        s1.addCourse("CPSC 2150");
+        s1.addCourse("CPSC 2150"); // duplicate test
+        s1.printCourses();
+        s1.removeCourse("CPSC 1010");
+        s1.removeCourse("MATH 1080"); // not enrolled test
+        s1.printCourses();
 
-        // after student class is created, i'll add scanner.next() for name, major and other info
-
-
+        // Test availabilities
+        System.out.println("\n=== Testing Availabilities ===");
+        s1.addAvailability("MWF", "8:00-8:50");
+        s1.addAvailability("MWF", "9:05-9:55");
+        s1.addAvailability("MWF", "9:05-9:55"); // duplicate test
+        s1.printAvailabilities();
+        s1.removeAvailability("MWF", "8:00-8:50");
+        s1.removeAvailability("MWF", "10:10-11:00"); // not found test
+        s1.printAvailabilities();
+         */
     }
-
 }
